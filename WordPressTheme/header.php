0cd1f8ff1d84d990cb2f54908c1ee9893a536f93
DESKTOP-0B9KPCU\Miki Itabashi <ita6.work@gmail.com>
2021-10-13 17:25:51 +0900

    コーディング前準備

--- WordPressTheme/header.php
+++ WordPressTheme/header.php
@@ -1,14 +1,20 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
+
 <head>
-  <meta charset="<?php bloginfo( 'charset' ); ?>">
+  <meta charset="<?php bloginfo('charset'); ?>">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta name="format-detection" content="telephone=no">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <!-- font awesome -->
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Amatic+SC:wght@400;700&family=Damion&family=Noto+Serif+JP:wght@400;500;600&family=Patua+One&display=swap" rel="stylesheet">
   <?php wp_head(); ?>
 </head>
+
 <body <?php body_class(); ?>>
   <?php wp_body_open(); ?>
   <header class="">
-    <p class="p-test">aaaa</p>
+    <p class="c-test">aaaa</p>
   </header>
