cc763a11d3959603cbfd7701acf2e6a998bc1dfc
DESKTOP-0B9KPCU\Miki Itabashi <ita6.work@gmail.com>
2022-01-18 21:52:14 +0900

    パーツ作成中

--- WordPressTheme/front-page.php
+++ WordPressTheme/front-page.php
@@ -1,5 +1,7 @@
 <?php get_header(); ?>
 
-front-page
+<a href="#" class="c-btn">詳しくはこちら</a>
 
+<a href="#" class="c-tab-btn">パスタ</a>
+<button href="#" class="c-tab-btn c-tab-btn--disable">パスタ</button>
 <?php get_footer(); ?>
